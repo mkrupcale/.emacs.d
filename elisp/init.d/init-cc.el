e526831f290a79f08e3ef4c20c5182e1df5a2ec1
Matthew Krupcale <mkrupcale@matthewkrupcale.com>
2017-06-22 13:51:41 -0700

    Add some CC Mode customizations
    * elisp/init.d/init-cc.el: basic style settings and mode setup (derived primarily from Edward E. Knyshov)
    * elisp/init.d/init-modes.el: load `init-cc`
    * init.el: `Custom.el` updates

--- elisp/init.d/init-cc.el
+++ elisp/init.d/init-cc.el
@@ -1,4 +1,4 @@
-;;; init-modes.el --- Init file to load Emacs modes
+;;; init-cc.el --- Init file to load Emacs CC mode
 ;;
 ;; Author: Matthew Krupcale <mkrupcale@matthewkrupcale.com>
 ;;
@@ -22,8 +22,37 @@
 ;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 ;; SOFTWARE.
 
-(load-library "init-cc")
-(load-library "init-markdown")
-(load-library "init-mcnp")
-(load-library "init-php")
-(load-library "init-yaml")
+(require 'req-package)
+
+;; clang format
+
+(req-package clang-format
+  :commands clang-format-region)
+
+;; completion with clang
+
+(defconst cc-style
+  '("bsd"
+    (c-offsets-alist . ((innamespace . [0])))))
+
+(req-package cc-mode
+  :loader :built-in
+  :config
+  (c-add-style "cc-style" cc-style)
+  (setq-default c-basic-offset 4)
+  (setq-default c-default-style "cc-style"))
+
+;; detect mode for .h file
+
+(req-package dummy-h-mode
+  :commands dummy-h-mode
+  :init (add-to-list 'auto-mode-alist '("\\.h$" . dummy-h-mode))
+  :config (progn
+            (add-hook-exec 'dummy-h-mode
+              (lambda ()
+                (setq dummy-h-mode-default-major-mode 'c++-mode)))
+            (add-hook-exec 'dummy-h-mode
+              (lambda ()
+                (setq dummy-h-mode-search-limit 60000)))))
+
+(provide 'init-cc)
--- init.el
+++ init.el
@@ -49,3 +49,17 @@
 
 ;; postpone real initialization until after ELPA is loaded
 (add-hook 'after-init-hook (lambda () (load-library "after-init")))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (dummy-h-mode clang-format yaml-mode php-mode markdown-mode req-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
