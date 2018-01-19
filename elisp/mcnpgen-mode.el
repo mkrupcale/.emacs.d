3c8923f102ea07c71cbe1b721894b406ce76cf3f
Matthew Krupcale <mkrupcale@matthewkrupcale.com>
2018-01-19 16:04:36 -0500

    Use SPDX license identifiers rather than verbose license headers. This should also help prevent confusion on what files are licensed as what.
    * COPYING.GPLv3: Add GPLv3 license file required by LGPLv3.
    
    * elisp/init.d/after-init.el: Use MIT license identifier.
    * elisp/init.d/init-backup-autosave.el: likewise.
    * elisp/init.d/init-cc.el: likewise.
    * elisp/init.d/init-ggtags.el: likewise.
    * elisp/init.d/init-helm.el: likewise.
    * elisp/init.d/init-mcnp.el: likewise.
    * elisp/init.d/init-modes.el: likewise.
    * elisp/init.d/init-php.el: likewise.
    * elisp/init.d/init-tidy.el: likewise.
    * init.el: likewise.
    
    * elisp/init.d/init-markdown.el: Use LGPL-3.0+ license identifier.
    * elisp/init.d/init-yaml.el: likewise.
    
    * elisp/mcnpgen-mode.el: Use GPL-2.0+ license identifier.

--- elisp/mcnpgen-mode.el
+++ elisp/mcnpgen-mode.el
@@ -1,3 +1,5 @@
+;; SPDX-License-Identifier: GPL-2.0+
+;;
 ;;Generic mode for highlighting syntax for LANL's 
 ;;MCNP Monte Carlo transport code input file.
 ;;
@@ -6,20 +8,6 @@
 ;;Address: University of Wisconsin-Madison
 ;;Department: Medical Physics
 ;;Copyright (C) 2002
-;;
-;;This program is free software; you can redistribute it and/or modify
-;;it under the terms of the GNU General Public License as published by
-;;the Free Software Foundation; either version 2 of the License, or
-;;(at your option) any later version.
-;;
-;;This program is distributed in the hope that it will be useful,
-;;but WITHOUT ANY WARRANTY; without even the implied warranty of
-;;MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;;GNU General Public License for more details.
-;;
-;;You should have received a copy of the GNU General Public License
-;;along with this program; if not, write to the Free Software
-;;Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;  
 ;;         How to use:
